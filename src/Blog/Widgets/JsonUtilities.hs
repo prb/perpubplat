b3df0e6d7da8f1a771af745f917b740ee44dd993
Paul Brown <prb@mult.ifario.us>
2008-09-24 13:24:45 -0700

    Tighten up the pattern matches to avoid non-exhaustive exceptions when garbage JSON comes through.

--- src/Blog/Widgets/JsonUtilities.hs
+++ src/Blog/Widgets/JsonUtilities.hs
@@ -31,35 +31,39 @@ empty_array = JSArray []
 
 unn_ :: JSValue -> [Int]
 unn_ a@(JSArray _) = map unn $ una . flatten $ a
+unn_ r@(JSRational _) = [ unn r ]
+unn_ v = error $ "Can't un-number-array a non-number-array value: " ++ (show v)
 
 unnWithDefault :: Int -> JSValue -> Int
-unnWithDefault i JSNull = i
-unnWithDefault i (JSArray []) = i
+unnWithDefault _ j@(JSRational _) = unn j
 unnWithDefault i (JSArray [j]) = unnWithDefault i j
-unnWithDefault _ j = unn j
+unnWithDefault i _ = i
 
 unn :: JSValue -> Int
 unn (JSRational n) = fromInteger . round $ n
 unn (JSArray [n@(JSRational _)]) = unn n
+unn v = error $ "Can't un-number a non-JSRational value: " ++ (show v)
 
 uno :: JSValue -> [(String,JSValue)]
 uno (JSObject o) = fromJSObject o
-uno _ = error "Can't un-object a non-JSOBject value."
+uno v = error $ "Can't un-object a non-JSOBject value: " ++ (show v)
 
 una :: JSValue -> [JSValue]
 una (JSArray a) = a
-una _ = error "Can't un-array a non-JSArray value."
+una v = error $ "Can't un-array a non-JSArray value: " ++ (show v)
 
 uns_ :: JSValue -> [String]
 uns_ a@(JSArray _) = map uns $ una . flatten $ a
+uns_ (JSString s) = [fromJSString s]
+uns_ n@(JSRational _) = [show . unn $ n]
+uns_ v = error $ "Can't un-string-array a non-JSArray value: " ++ (show v)
 
 uns :: JSValue -> String
 uns (JSString s) = fromJSString s
 uns (JSArray [s@(JSString _)]) = uns s
-uns _ = error "Can't un-string non-JSString value."
+uns v = error $ "Can't un-string non-JSString value: " ++ (show v)
 
 unsWithDefault :: String -> JSValue -> String
-unsWithDefault s JSNull = s
-unsWithDefault s (JSArray []) = s
+unsWithDefault _ v@(JSString _) = uns v
 unsWithDefault s (JSArray [v]) = unsWithDefault s v
-unsWithDefault _ v = uns v
+unsWithDefault s _ = s
